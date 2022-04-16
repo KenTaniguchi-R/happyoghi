32b882e79638eb6f7c9f5f3cb6fed442b03aeb39
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2022-04-16 13:55:27 -0700

    added admin

--- foods_container/admin.py
+++ foods_container/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from foods_container.models import Container
+
+class ContainerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Container, ContainerAdmin)
