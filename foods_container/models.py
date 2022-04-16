f1f3a0dbd799518f34502f6e9a7247e1c1f356c1
RyuseiTaniguchi <taniguchi.ryusei@gmail.com>
2022-04-16 13:51:49 -0700

    created model

--- foods_container/models.py
+++ foods_container/models.py
@@ -1,3 +1,13 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+import uuid
+
+from customer.models import Customer
+
 # Create your models here.
+
+class Container(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    foods = models.JSONField(verbose_name="foods and counter")
